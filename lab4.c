48c0737061fa455c680625f7e5dcfc6c595cfe00
Ryder Dale Walton <rdnotlaw91@outlook.com>
2018-02-02 09:59:53 -0600

    restructured usable code from last lab

--- lab4.c
+++ lab4.c
@@ -5,38 +5,45 @@
 #include <unistd.h>
 #include <time.h>
 
-#define NUM_NODES 30
-#define MAX 2147483647
+#define NUM_NODES  3       // Initial number of nodes, chunks of memory
+#define BLOCK_SIZE 1024    // Maximum block size
 #define DEBUG
 
 /* type definitions */
 // these nodes will be strung together in a doubly-linked queue
-typedef struct _node {
-    int ptid;             // process/thread ID
-    int prio;             // priority. Lower # is higher priority
-    char desc[100];        // description of process/thread
+typedef struct _node 
+{
+    int ptid;              // node ID
+    int nBase;             // the base register memory offset of the node
+    int nStay;             // the length of time the node has been allocated
+    int nBlocks;           // the limit register of the memory process
     struct _node *next;    // pointer to next node in list
     struct _node *prev;    // pointer to previous node in list
 } node;
 
+// used to form the memory allocated and available queues
+typedef struct _doubly_linked_queue
+{
+    node *head, *tail, *current;
+    int length;
+} doubly_linked_queue;
+
 /* function prototypes */
-void init_queue();
+void init_queues();
 void init_node();
 void enqueue();
 bool is_empty();
-void silent_traverse();
 void requeue();
-void enqueue_dispatched();
-void *schedule();
-void *interrupt();
+node *dequeue();
+void *allocate();
+void *collect();
 void *traverse();
+void *increment_times();
 
 /* global variables */
-node *CURRENT  = NULL;
-node *HEAD     = NULL;
-node *TAIL     = NULL;
-node *MIN_PRIO = NULL;
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+doubly_linked_queue *AVAILABLE_MEMORY = NULL;
+doubly_linked_queue *ALLOCATED_MEMORY = NULL;
 int i, rc;
 
 /* methods */
@@ -45,17 +52,18 @@ int i, rc;
 int main(int argc, char *argv[]) 
 {
     srand(time(NULL));
-    // initialize doubly-linked queue
-    init_queue();
+    
+    // initialize doubly-linked queues
+    init_queues();
     
     // create the threads
-    pthread_t scheduler, interrupter, traverser;
-    if ((rc = pthread_create(&scheduler, NULL, schedule, NULL)))
+    pthread_t allocator, collector, traverser, timer;
+    if ((rc = pthread_create(&allocator, NULL, allocate, NULL)))
     {
         fprintf(stderr, "error: scheduler thread creation, rc: %d\n", rc);
         return EXIT_FAILURE;
     }
-    if ((rc = pthread_create(&interrupter, NULL, interrupt, NULL)))
+    if ((rc = pthread_create(&collector, NULL, collect, NULL)))
     {
         fprintf(stderr, "error: interrupt thread creation, rc: %d\n", rc);
         return EXIT_FAILURE;
@@ -65,26 +73,35 @@ int main(int argc, char *argv[])
         fprintf(stderr, "error: traverser thread creation, rc: %d\n", rc);
         return EXIT_FAILURE;
     }
+    if ((rc = pthread_create(&timer, NULL, increment_times, NULL)))
+    {
+        fprintf(stderr, "error: timer thread creation, rc: %d\n", rc);
+    }
 
     // block for thread completion before exiting
-    pthread_join(scheduler, NULL);
-    pthread_join(interrupter, NULL);
+    pthread_join(allocator, NULL);
+    pthread_join(collector, NULL);
     pthread_join(traverser, NULL);
+    pthread_join(timer, NULL);
 
     return EXIT_SUCCESS;
 }
 
 // Creates a doubly-linked queue with initialized nodes
-void init_queue()
+void init_queues()
 {
+    AVAILABLE_MEMORY = (doubly_linked_queue*) malloc(sizeof(doubly_linked_queue));
+    if (AVAILABLE_MEMORY == NULL) return;
+    ALLOCATED_MEMORY = (doubly_linked_queue*) malloc(sizeof(doubly_linked_queue));
+    if (ALLOCATED_MEMORY == NULL) return;
     for (i = 0; i < NUM_NODES; ++i)
     {
         node *new_node = (node*) malloc(sizeof(node));
-        if (new_node == NULL) return;
+        if (new_node == NULL) return; // check to see if heap is full (malloc failure)
         init_node(new_node, i);    
-        enqueue(new_node);
+        enqueue(AVAILABLE_MEMORY, new_node);
 #ifdef DEBUG       
-       printf("New node ptid: %d prio: %d\n", new_node->ptid, new_node->prio);
+        printf("New node ptid: %d nBase: %d nStay: %d nBlocks: %d\n", new_node->ptid, new_node->nBase, new_node->nStay, new_node->nBlocks);
 #endif        
     }
 }
@@ -93,144 +110,148 @@ void init_queue()
 void init_node(node *new_node, int i)
 {
     new_node->ptid = i;
-    new_node->prio = rand() % 70 + 31; // values 30-100
-    new_node->desc[0] = 'n';
-    new_node->desc[1] = '\0'; // plan to do more with desc if time allows   
+    new_node->nBase = i * BLOCK_SIZE; // not exactly right
+    new_node->nStay = 0;
+    new_node->nBlocks = BLOCK_SIZE;   
 }
 
-// add new node to the end of the queue
-void enqueue(node *new_node)
+// add new node to the end of the passed in queue
+void enqueue(doubly_linked_queue *memory, node *new_node)
 {
-    if (!is_empty()) 
+    if (!is_empty(memory)) 
     { 
-        TAIL->next = new_node;
-        new_node->prev = TAIL;
+        memory->tail->next = new_node;
+        new_node->prev = memory->tail;
     }
-    else
-        HEAD = new_node;
-    TAIL = new_node; 
+    else 
+        memory->head = new_node;
+    memory->tail = new_node; 
 
 #ifdef DEBUG
-    printf("TAIL ptid: %d HEAD ptid: %d\n", TAIL->ptid, HEAD->ptid);
+    printf("queue tail ptid: %d queue head ptid: %d\n", memory->tail->ptid, memory->head->ptid);
 #endif
   
 }
 
-// helper function that adds the min prio node
-// back to the end of the list
-void enqueue_dispatched()
-{
-    TAIL->next = MIN_PRIO;
-    MIN_PRIO->prev = TAIL;
-    TAIL = MIN_PRIO;
-}
-
 // determines if the doubly-linked queue is empty
-bool is_empty() 
+bool is_empty(doubly_linked_queue *memory) 
 {
-    return HEAD == NULL;
+    return memory == NULL || memory->head == NULL;
 }
 
-// traverses the queue, returning a pointer to the
-// node with the highest priority
-void silent_traverse() 
+// removes the memory node from its location in one queue
+// and replaces it at the end of the other queue
+void requeue(doubly_linked_queue *memory, node *a_node)
 {
-    int min = MAX;
-    CURRENT = HEAD;
-    while(CURRENT != TAIL) // tail not considered
-    {
-        if (CURRENT->prio < min)
-        {
-            min = CURRENT->prio;
-            MIN_PRIO = CURRENT;
-        }
-        CURRENT = CURRENT->next;
-    }
-}
-
-// removes the scheduled node from its location in the queue
-// and replaces it at the end of the queue
-void requeue()
-{
-    node *previous = MIN_PRIO->prev;
-    node *next = MIN_PRIO->next;
+    node *previous = a_node->prev;
+    node *next = a_node->next;
     previous->next = next;
     next->prev = previous;
-    enqueue_dispatched();
+    enqueue(memory, a_node);
+}
+
+// removes a node from a memory queue and returns it
+node *dequeue(doubly_linked_queue *memory, node *a_node)
+{
+    node *previous = a_node->prev;
+    node *next = a_node->next;
+    previous->next = next;
+    next->prev = previous;
+    return a_node;
 }
 
 /* thread methods */
-void *schedule()
+// select a node from the available queue to allocate
+// based a randomly generated number. The number should
+// be compared to the nBlocks value of the node. If the
+// nBlocks value is not too large, use that node. If it
+// is too large, split the node: Decrement the nBlocks
+// value of the existing node by the amount that will be
+// allocated. Create a new node with the random number
+// as the nBlocks size. Enqueue this node in the allocated
+// memory queue.
+void *allocate()
 {
     while(true) 
     {
 #ifdef DEBUG
-        printf("\n==== Scheduling Process ====\n");
+        printf("\n==== Allocating Memory ====\n");
 #endif
-        // find the minimum priority with a traversal
-        // print this node's information out
-        // adjust the node's priority 
-        // place it at the end of the queue 
         pthread_mutex_lock(&mutex);
-        silent_traverse();
-        printf("SCHEDULED: Node %d with Prio %d and Desc %s\n", MIN_PRIO->ptid, MIN_PRIO->prio, MIN_PRIO->desc);
-        MIN_PRIO->prio = rand() % 70 + 31; // 30-100
-        requeue();
         pthread_mutex_unlock(&mutex);       
-        sleep(10);
+        sleep(1);
     }
     return EXIT_SUCCESS;
 }
 
-// Randomly chooses node(s) every minute
-// and changes priority.
-void *interrupt()
+// Garbage collects an allocated node and move is back to the available queue
+void *collect()
 {
     while(true)
     {
 #ifdef DEBUG
-        printf("\n==== Interrupting Processes ====\n");
+        printf("\n==== Garbage Collecting ====\n");
 #endif
-        int steps = rand() % 30;          // 0 - 29
         pthread_mutex_lock(&mutex);
-        CURRENT = HEAD;
-        for(i = 0; i < steps && i < NUM_NODES; ++i)
-        {
-            CURRENT = CURRENT->next;
-        }
-#ifdef DEBUG
-        printf("Node %d Priority BEFORE update: %d\n", CURRENT->ptid, CURRENT->prio);
-#endif
-        CURRENT->prio = rand() % 70 + 31; // 30 - 100
-#ifdef DEBUG
-        printf("Node %d Priority AFTER update: %d\n", CURRENT->ptid, CURRENT->prio);
-#endif
         pthread_mutex_unlock(&mutex);
-        sleep(60);
+        sleep(2);
     }
     return EXIT_SUCCESS;
 }
 
-// traverses through the queue starting at the head
-// printing out a list of nodes as identified by the
-// node's ptid
+// traverses through the queues starting at the head. Print out info.
 void *traverse()
 {
     while(true) 
     {
-#ifdef DEBUG
-        printf("\n==== Traversing Doubly-Linked Queue ====\n");
-#endif
-        pthread_mutex_lock(&mutex);
-        CURRENT = HEAD;
-        while(CURRENT != TAIL)
+        if (AVAILABLE_MEMORY != NULL && !is_empty(AVAILABLE_MEMORY))
         {
-            printf("Current node: %d\n", CURRENT->ptid);
-            CURRENT = CURRENT->next;
+#ifdef DEBUG
+            printf("\n==== Traversing Available Memory ====\n");
+#endif
+            pthread_mutex_lock(&mutex);
+            AVAILABLE_MEMORY->current = AVAILABLE_MEMORY->head;
+            while(AVAILABLE_MEMORY->current != AVAILABLE_MEMORY->tail)
+            {
+                printf("Current node: %d\n", AVAILABLE_MEMORY->current->ptid);
+                AVAILABLE_MEMORY->current = AVAILABLE_MEMORY->current->next;
+            }
+            printf("Current node: %d\n", AVAILABLE_MEMORY->tail->ptid); // must print the tail too 
+            pthread_mutex_unlock(&mutex);
         }
-        printf("Current node: %d\n", TAIL->ptid); // must print the tail too 
-        pthread_mutex_unlock(&mutex);
-        sleep(20);
+        else
+            printf("AVAILABLE_MEMORY IS EMPTY\n");
+
+        if (ALLOCATED_MEMORY != NULL && !is_empty(ALLOCATED_MEMORY))
+        {
+#ifdef DEBUG
+            printf("\n==== Traversing Allocated Memory ====\n");
+#endif
+            pthread_mutex_lock(&mutex);
+            ALLOCATED_MEMORY->current = ALLOCATED_MEMORY->head;
+            while(ALLOCATED_MEMORY->current != ALLOCATED_MEMORY->tail)
+            {
+                printf("Current node: %d\n", ALLOCATED_MEMORY->current->ptid);
+                ALLOCATED_MEMORY->current = ALLOCATED_MEMORY->current->next;
+            }
+            printf("Current node: %d\n", ALLOCATED_MEMORY->tail->ptid); // must print the tail too 
+            pthread_mutex_unlock(&mutex);
+        } 
+        else
+            printf("ALLOCATED MEMORY IS EMPTY.\n");
+        sleep(5);
     }
     return EXIT_SUCCESS;
 }
+
+// increments the nStay value of each node in the allocated queue
+void *increment_times()
+{
+    while(true)
+    {
+        // if the allocated_memory isn't empty, increment all of the
+        // nodes' stay value
+    }
+    return EXIT_SUCCESS;
+}
+
